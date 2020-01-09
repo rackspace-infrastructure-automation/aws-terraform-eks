5e9af08f0219b564534e504987734f7df3c270bd
John Titus <john.titus@rackspace.com>
2020-01-09 13:54:33 -0600

    Start using default VPC for EKS tests

--- tests/test1/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kubeconfig" {
-  description = "output of the kubeconfig"
-  value       = "${module.eks.kubeconfig}"
-}
-
-output "aws_auth_cm" {
-  description = "output of the aws auth"
-  value       = "${module.eks.aws_auth_cm}"
-}
